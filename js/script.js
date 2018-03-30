e37fe4f24e9bb83890b694ee879bfec4c6dca162
marina_cell <melissa_nsk_@mail.ru>
2018-03-30 23:07:40 +0700

    Подправила предыдущий коммит

--- js/script.js
+++ js/script.js
@@ -6,35 +6,7 @@ var username = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=email]");
 var message = popup.querySelector("[name=message]");
 var background = document.querySelector(".site-wrapper");
-var radio_buttons = document.querySelectorAll(".switch-button");
 
-//console.log(radio_buttons);
-//  for(var i = 0; i < 3; i++) {
-//    radio_buttons[i].addEventListener("click", function (evt) {
-//      evt.preventDefault();
-//      background.classList.remove("slide-1");
-//      background.classList.remove("slide-2");
-//      background.classList.remove("slide-3");
-//      radio_buttons[i].classList.add("switch-button-current");
-//      
-//      if(i == 0) {
-//         background.classList.add("slide-1");
-//         
-//      }
-//      else if(i == 1) {
-//        background.classList.add("slide-2");
-//       
-//      }
-//      else if(i == 2) {
-//        background.classList.add("slide-3");
-//      }
-//    });
-//  }
-
-
-                                      
-  
-  
 var isStorageSupport = true;
 
 var storage_username = "";
